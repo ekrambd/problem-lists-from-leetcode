7cc546d32528c2a5742196aa1603087aca6d4d14
ekrambd <ekramhossainekram28@gmail.com>
2026-04-22 21:07:34 +0600

    leetcode 242 problem fix

--- leetcode_242.js
+++ leetcode_242.js
@@ -2,19 +2,20 @@
 
 var isAnagram = function(s, t) {
     let map = new Map();
-    if(s.length != t.length) return false;
-    for(let i = 0; i < s.length; i++)
-    {
-        let count = map.get(s[i]);
-        map.set(s[i],count+1);
-    }
-    for(let i = 0; i < s.length; i++)
-    {
-        let count = map.get(t[i]);
-        if(count == 0) return false;
-        map.set(t[i],count-1);
-    }
-    return true;        
+    if (s.length !== t.length) return false;
+
+	for (let i = 0; i < s.length; i++) {
+		let count = map.get(s[i]) || 0;
+		map.set(s[i], count + 1);
+	}
+
+	for (let i = 0; i < t.length; i++) {
+		let count = map.get(t[i]) || 0;
+		if (count === 0) return false;
+		map.set(t[i], count - 1);
+	}
+
+	return true;       
 };
 
 console.log(isAnagram("anagram", "nagaram"));
