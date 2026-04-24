28fb7c8b765322ab140be85f78a37679de89be0e
ekrambd <ekramhossainekram28@gmail.com>
2026-04-24 14:22:49 +0600

    leetcode 13 solved

---
+++ leetcode_13.js
@@ -0,0 +1,28 @@
+//13. Roman to Integer
+
+var romanToInt = function(s) {
+    let map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let total = 0;
+    for(let i = 0; i < s.length; i++)
+    {
+        let current = map[s[i]];
+        let next    = map[s[i+1]];
+        if(current < next){
+            total-=current;
+        }else{
+            total+=current;
+        }
+    }
+    return total;        
+};
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
